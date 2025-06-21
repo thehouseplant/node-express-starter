6ac8d9622832205d9ed9318827809beb82e826e1
Sean Collins <sean.collins@outlook.com>
2025-06-20 17:27:43 -0700

    Import Helmet with default configuration and add some comments

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import helmet from 'helmet';
 import employeeRoutes from './routes/employeeRoutes';
 
 const app = express();
 
+// Initialize helmet
+app.use(helmet());
+
+// Express JSON parser
 app.use(express.json());
 
-// Routes
+// Import routes
 app.use('/api/v1/employees', employeeRoutes);
 
 export default app;
